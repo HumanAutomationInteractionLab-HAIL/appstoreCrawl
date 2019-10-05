822089dce6c6a986e609860bab677b68d6e88d86
Yongkang Li <51050114+SocraLee@users.noreply.github.com>
2019-10-05 19:10:41 +0800

    微调代码；附加结果
    为代码提供了输入入口。
    添加了多个app分列好的评论、切词结果、词频统计结果

--- CutOnly/WordCutOnly.py
+++ CutOnly/WordCutOnly.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import jieba
 import xlrd
-def readInAndHandle(stopwords):
-    data=xlrd.open_workbook("Comments.xlsx",encoding_override='utf-8')
+def readInAndHandle(stopwords,name):
+    data=xlrd.open_workbook("CommentsOf"+name+".xlsx",encoding_override='utf-8')
     table=data.sheets()[0]
     nrows = table.nrows  # 获取行号
     ncols = table.ncols  # 获取列号
@@ -11,10 +11,10 @@ def readInAndHandle(stopwords):
         alldata = table.row_values(i)  # 循环拿到excel表中每一行，即所有数据
         result = str(alldata[5])  # 取出表中第二列数据
         print(result)
-        handleAndWrite(result,stopwords)
+        handleAndWrite(result,stopwords,name)
 
-def handleAndWrite(comment,stopWords):
-    with open('D:/WordsOf海尔智家.txt','a',encoding='utf-8')as file:
+def handleAndWrite(comment,stopWords,name):
+    with open('WordsOf'+name+'.txt','a',encoding='utf-8')as file:
         commentWord=jieba.cut(comment)
         strToWrite=''
         for word in commentWord:
@@ -26,6 +26,8 @@ def handleAndWrite(comment,stopWords):
     file.close()
 def main():
     stopwords = [line.strip() for line in open('StopwordOfBaidu.txt', encoding='UTF-8').readlines()]
-    readInAndHandle(stopwords)
+    name=input()
+    #输入需要分析的app的名字
+    readInAndHandle(stopwords,name)
 if __name__ == '__main__':
     main()
